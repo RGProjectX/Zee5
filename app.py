25c2c60753035555114ff76f957520e1c224da46
RG-ProjectX <69814166+RGProjectX@users.noreply.github.com>
2021-11-10 23:39:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = FastAPI()
 
 @app.get("/{channel}")
 def read_root(channel: str):
-        try:
-	    stream = g(f'https://catalogapi.zee5.com/v1/channel/{channel}').json()['stream_url_hls']
-            token = g('https://useraction.zee5.com/token/live.php').json()['video_token']
-            rr(f'{stream}{token}')
-        except Exception as e:
-            return e
+	try:
+		stream = g(f'https://catalogapi.zee5.com/v1/channel/{channel}').json()['stream_url_hls']
+        token = g('https://useraction.zee5.com/token/live.php').json()['video_token']
+        rr(f'{stream}{token}')
+	except Exception as e:
+    	return e
