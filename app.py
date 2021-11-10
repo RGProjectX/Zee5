645239d65e276a59899343b7b7f8c52c09404442
RG-ProjectX <69814166+RGProjectX@users.noreply.github.com>
2021-11-10 23:14:00 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from requests import get as g
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/{channel}")
+
+def read_root(channel: int):
+
+	stream = g(f'https://catalogapi.zee5.com/v1/channel/{channel}').json()['stream_url_hls']
+
+	token = g('https://useraction.zee5.com/token/live.php').json()['video_token']
+
+	return(f'{stream}{token}')
