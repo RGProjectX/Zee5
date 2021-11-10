776d2ab893d52c3be0b32866ba3545fb2e85edb3
RG-ProjectX <69814166+RGProjectX@users.noreply.github.com>
2021-11-10 23:31:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from requests import get as g
-
+from fastapi.responses import RedirectResponse as rr
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,9 +7,11 @@ app = FastAPI()
 @app.get("/{channel}")
 
 def read_root(channel: str):
-
+    try:
 	stream = g(f'https://catalogapi.zee5.com/v1/channel/{channel}').json()['stream_url_hls']
 
 	token = g('https://useraction.zee5.com/token/live.php').json()['video_token']
 
-	return(f'{stream}{token}')
+	rr(f'{stream}{token}')
+    except Exception as e:
+        return e
